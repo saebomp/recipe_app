67c34cdcf0d315deefc6b280ee05894f182adeab
SAEBOMPAK <saebompak@gmail.com>
2020-10-24 13:57:01 +0900

    fetching api

--- src/services/api.js
+++ src/services/api.js
@@ -2,4 +2,19 @@ import axios from 'axios';
 
 import {APP_ID, APP_KEY, BASE_URL} from '../config/api_config'
 
-// const getRecipes = async
+export const getRecipes = async (recipeName) => {
+  const url = BASE_URL;
+  try {
+    const response = await axios.get(url, {
+      params:{
+        q:recipeName,
+        app_id:APP_ID,
+        app_key:APP_KEY
+      }
+    })
+    console.log('response', response)
+    } 
+    catch(error) {
+      throw error
+  }
+}
